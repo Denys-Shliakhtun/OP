83845c4e54be4f11dd41b060acfcf6b1ae5ccc16
Denys-Shliakhtun <denys.shlakhtun@gmail.com>
2021-09-27 17:17:18 +0300

    Lab1

---
+++ lab1/lab1/lab1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main()
+{
+    int number1; //тризначне число
+    std::cout << "Enter the number: "; std::cin >> number1; //введення тризначного числа
+    int left = number1 / 100; //визначення лівої цифри
+    int number2 = (number1 - left * 100) * 10 + left; //кінцевий результат, переміщення лівої цифри в кінець
+    std::cout << "Result: " << number2; //виведення результату
+}
+
+
